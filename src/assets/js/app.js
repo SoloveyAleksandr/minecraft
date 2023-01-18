ae14553b5f4f3dc3407934d962b26282e9999f0b
Aleksandr <solalex.s.a@gmail.com>
2023-01-18 17:51:36 +0300

    add partners

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // MAIN
+  if (document.querySelector('.main-partners-swiper')) {
+    const swiper = new Swiper('.main-partners-swiper', {
+      effect: 'slide',
+      slidesPerView: 6,
+      direction: 'horizontal',
+      spaceBetween: 30,
+      autoplay: true,
+      loop: true,
+      freeMode: true,
+    })
+  }
 
+  new Swiper('.swiper', {})
+  //<==
 })
