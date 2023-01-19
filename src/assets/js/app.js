3a4fd3f73c374b8c668965cba9056efeaa68d223
Aleksandr <solalex.s.a@gmail.com>
2023-01-19 10:40:18 +0300

    add animation

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,17 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
   // MAIN
-  if (document.querySelector('.main-partners-swiper')) {
-    const swiper = new Swiper('.main-partners-swiper', {
-      effect: 'slide',
-      slidesPerView: 6,
-      direction: 'horizontal',
-      spaceBetween: 30,
-      autoplay: true,
-      loop: true,
-      freeMode: true,
+  if (document.querySelector('.main-partners')) {
+    const container = document.querySelector('.main-partners-list-container');
+    const listNode = document.querySelector('.main-partners-list');
+    container.appendChild(listNode.cloneNode(true));
+
+    const animLists = gsap.utils.toArray('.main-partners-list');
+    const speed = listNode.children.length * 2.5;
+    const TL = gsap.timeline({
+      repeat: -1,
     })
+
+    animLists.forEach(list => {
+      TL.to(list, {
+        x: '-100%',
+        ease: 'none',
+        duration: speed,
+      }, 'sin')
+    });
   }
 
-  new Swiper('.swiper', {})
+  if (document.querySelector('.main-reviews')) {
+    const container = document.querySelector('.main-reviews-list-container');
+    const listNode = document.querySelector('.main-reviews-list');
+    container.appendChild(listNode.cloneNode(true));
+    const animLists = gsap.utils.toArray('.main-reviews-list');
+    const speed = listNode.children.length * 4;
+
+    const TL = gsap.timeline({
+      repeat: -1,
+    })
+
+    animLists.forEach(list => {
+      TL.to(list, {
+        x: '-100%',
+        ease: 'none',
+        duration: speed,
+      }, 'sin')
+    });
+  }
   //<==
 })
